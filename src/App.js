b53d95e8b9eee0637592a5b9c097457fa8435227
Arun <kingarun2020@gmail.com>
2023-06-14 12:34:46 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
 
-function App() {
+
+
+
+const App = () => {
+
+  const [data,setData]=useState([])
+useEffect(()=>{
+  fetch("https://jsonplaceholder.typicode.com/users").then(response=>{
+  response.json().then(result=>{
+    setData(result)
+  })
+})
+},[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Fetching Data from API using Fetch ,UseEffect and UseState</p>
+      <table border='1px'>
+        <tbody>
+        <tr>
+          <td>ID</td>
+          <td>Name</td>
+          <td>Email</td>
+          <td>Mobile Number</td>
+
+        </tr>
+        
+       {
+        data.map((item)=>{
+         return(
+         <tr>
+          <td>{item.id}</td>
+          <td>{item.name}</td>
+          <td>{item.email}</td>
+          <td>{item.phone}</td>
+
+        </tr>)
+
+        })
+       }
+       </tbody>
+      </table>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
